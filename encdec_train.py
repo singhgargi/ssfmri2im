91468cacd1c6540cfaeefd097bbc23e35666b9a5
singhgargi <40858250+singhgargi@users.noreply.github.com>
2020-07-23 23:05:45 +0530

    Update encdec_train.py

--- encdec_train.py
+++ encdec_train.py
@@ -23,19 +23,21 @@ set_session(tf.Session(config=config))
 
 
 #################################################### data load #########################################################
-# with open('data112.p', 'rb') as fp:
-#     data = pickle.load(fp)
-# Y = data['responses']
-# Y_test_avg = data['responses_test']
-# Y_test = data['responses_test']
-# labels = np.arange(Y_test.shape[0])
-# NUM_VOXELS = Y.shape[1]
-# X = data['stimuli']
-# X_test = data['stimuli_test']
-# X_test_avg = X_test
-handler = data_handler(matlab_file = config_file.kamitani_data_mat)
-Y,Y_test,Y_test_avg = handler.get_data(roi = 'ROI_VC',imag_data = 0)
-labels_train, labels = handler.get_labels(imag_data = 0)
+with open('data100.p', 'rb') as fp:
+    data = pickle.load(fp)
+Y = data['responses']
+Y_test_avg = data['responses_test']
+Y_test = data['responses_test']
+labels = np.arange(Y_test.shape[0])
+NUM_VOXELS = Y.shape[1]
+X = data['stimuli']
+X_test = data['stimuli_test']
+X_test_avg = X_test
+
+# handler = data_handler(matlab_file = config_file.kamitani_data_mat)
+# Y,Y_test,Y_test_avg = handler.get_data(roi = 'ROI_VC',imag_data = 0)
+# labels_train, labels = handler.get_labels(imag_data = 0)
+
 print("For Y")
 print(np.shape(Y))
 print("For Y_test")
@@ -47,17 +49,17 @@ print(np.shape(Y_test_avg))
 print("labels")
 print(np.shape(labels))
 
-file= np.load(config_file.images_npz) #_56
-X = file['train_images']
-X_test_avg = file['test_images']
+# file= np.load(config_file.images_npz) #_56
+# X = file['train_images']
+# X_test_avg = file['test_images']
 
 print("X")
 print(np.shape(X))
 print("X_test_avg")
 print(np.shape(X_test_avg))
 
-X= X[labels_train]
-X_test = X_test_avg[labels]
+# X= X[labels_train]
+# X_test = X_test_avg[labels]
 
 print("X")
 print(np.shape(X))
