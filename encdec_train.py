d851cdcffac3b25b6a654b631ba55ae5ac9b8495
singhgargi <40858250+singhgargi@users.noreply.github.com>
2020-07-27 00:48:11 +0530

    Update encdec_train.py

--- encdec_train.py
+++ encdec_train.py
@@ -25,17 +25,17 @@ set_session(tf.Session(config=config))
 #################################################### data load #########################################################
 with open('../gdrive/My Drive/datanorm01112.p', 'rb') as fp:
     data = pickle.load(fp)
-Y = data['responses']
-Y_test_avg = data['responses_test'][:100,:]
-Y_test = data['responses_test'][:100,:]
+Y = data['responses'][:1600,:]
+Y_test_avg = data['responses_test'][:50,:]
+Y_test = data['responses_test'][:50,:]
 ysam = Y_test
 for i in range(34):
     Y_test = np.concatenate((Y_test,ysam),axis=0)
 labels = np.arange(ysam.shape[0])
 labels = np.tile(labels,35)
 NUM_VOXELS = Y.shape[1]
-X = data['stimuli']
-X_test = data['stimuli_test'][:100,:]
+X = data['stimuli'][:1600,:]
+X_test = data['stimuli_test'][:50,:]
 X_test_avg = X_test
 xsam = X_test
 for i in range(34):
