84362b333f4b9d7aa77e35f892adbecf8c5f891f
singhgargi <40858250+singhgargi@users.noreply.github.com>
2020-07-27 11:46:46 +0530

    Update encoder_train.py

--- encoder_train.py
+++ encoder_train.py
@@ -35,10 +35,10 @@ set_session(tf.Session(config=gpu_config))
 with open('../gdrive/My Drive/datanorm01112.p', 'rb') as fp:
     data = pickle.load(fp)
 Y = data['responses'][:1500,:]
-Y_test_avg = data['responses_test'][:50,:]
+Y_test_avg = data['responses'][1500:1550,:]
 NUM_VOXELS = Y.shape[1]
 X = data['stimuli'][:1500,:]
-X_test = data['stimuli_test'][:50,:]
+X_test = data['stimuli'][1500:1550,:]
 X_test_sorted = X_test
 
 # handler = data_handler(matlab_file = config_file.kamitani_data_mat)
