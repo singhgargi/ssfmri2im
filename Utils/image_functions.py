a32896182adf257a96687ce2723cb4094a2d0d96
singhgargi <40858250+singhgargi@users.noreply.github.com>
2020-07-28 22:20:34 +0530

    Update image_functions.py

--- Utils/image_functions.py
+++ Utils/image_functions.py
@@ -129,24 +129,24 @@ def calc_accuracy(original_imgs,reconstructed_img,gt_idx):
 		return False
             
 def save_results(images,images_orig = None ,folder=''):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    res = {2:[],5:[],10:[]}
-for i in range(images.shape[0]):
-	for j in [2,5,10]:
-		gt_idx = j-1
-		new = np.delete(np.arange(images.shape[0]),i)
-		index = np.random.choice(new, j-1, replace=False)
-		index = np.append(index,i)
-		print("image index",i)
-		res[j].append(calc_accuracy(images_orig[index],images[i],gt_idx))
-    print("Total number of images used in 2-way",len(res[2]))
-    print("2-way accuracy",res[2].count(True)/len(res[2]))
-    print("Total number of images used in 5-way",len(res[5]))
-    print("5-way accuracy",res[5].count(True)/len(res[5]))
-    print("Total number of images used in 10-way",len(res[10]))
-    print("10-way accuracy",res[10].count(True)/len(res[10]))
-    with open(folder+'results.p', 'wb') as fp:
-        pickle.dump(res, fp, protocol=pickle.HIGHEST_PROTOCOL)
+	if not os.path.exists(folder):
+		os.makedirs(folder)
+	res = {2:[],5:[],10:[]}
+	for i in range(images.shape[0]):
+		for j in [2,5,10]:
+			gt_idx = j-1
+			new = np.delete(np.arange(images.shape[0]),i)
+			index = np.random.choice(new, j-1, replace=False)
+			index = np.append(index,i)
+			print("image index",i)
+			res[j].append(calc_accuracy(images_orig[index],images[i],gt_idx))
+	print("Total number of images used in 2-way",len(res[2]))
+	print("2-way accuracy",res[2].count(True)/len(res[2]))
+	print("Total number of images used in 5-way",len(res[5]))
+	print("5-way accuracy",res[5].count(True)/len(res[5]))
+	print("Total number of images used in 10-way",len(res[10]))
+	print("10-way accuracy",res[10].count(True)/len(res[10]))
+	with open(folder+'results.p', 'wb') as fp:
+		pickle.dump(res, fp, protocol=pickle.HIGHEST_PROTOCOL)
             
         
