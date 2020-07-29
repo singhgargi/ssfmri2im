9e9124bf01a2041879e6e87a9fdcd39e72892e4e
singhgargi <40858250+singhgargi@users.noreply.github.com>
2020-07-30 00:53:03 +0530

    Update encoder_train.py

--- encoder_train.py
+++ encoder_train.py
@@ -33,7 +33,7 @@ set_session(tf.Session(config=gpu_config))
 
 #################################################### data load ##########################################################
 
-with open('../gdrive/My Drive/data01112.p', 'rb') as fp:
+with open('../gdrive/My Drive/data01v1v2v3.p', 'rb') as fp:
     data = pickle.load(fp)
 Y = data['responses']
 Y_test_avg = data['responses_test']
