0c403d2b3e05d6e5254c0f152979474616e9f06f
singhgargi <40858250+singhgargi@users.noreply.github.com>
2020-07-29 15:27:20 +0530

    Update encoder_train.py

--- encoder_train.py
+++ encoder_train.py
@@ -34,10 +34,10 @@ set_session(tf.Session(config=gpu_config))
 
 with open('../gdrive/My Drive/data01v1v2v3v3av3b.p', 'rb') as fp:
     data = pickle.load(fp)
-Y = data['responses'][:1200,:]
+Y = data['responses'][:1500,:]
 Y_test_avg = data['responses_test'][:50,:]
 NUM_VOXELS = Y.shape[1]
-X = data['stimuli'][:1200,:]
+X = data['stimuli'][:1500,:]
 X_test = data['stimuli_test'][:50,:]
 X_test_sorted = X_test
 
